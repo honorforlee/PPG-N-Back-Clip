0857e35302214da772a5522ff941aa95703ff3e3
iROCKBUNNY <irockbunny@outlook.com>
2017-07-18 01:49:04 +0800

    Update

--- classify.py
+++ classify.py
@@ -1,1 +1,3 @@
 # -*- coding: utf-8 -*-
+
+from common import get_base_dir
---
+++ common.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+
+def get_base_dir():
+    return os.path.abspath(os.path.dirname(__file__))
+
+
+def make_dirs_for_file(filename):
+    try:
+        os.makedirs(os.path.split(filename)[0])
+    except:
+        pass
+
+
+def exist_file(filename, overwrite=False, display_info=True):
+    if os.path.exists(filename):
+        if overwrite:
+            os.remove(filename)
+            if display_info:
+                print u'File: %s exists. Remove: overwrite old file.' % filename
+            return False
+        else:
+            if display_info:
+                print u'File: %s exists. Skip: no new file is created.' % filename
+            return True
+    else:
+        if display_info:
+            print u'File: %s does not exist. Create new file. ' % filename
+        return False
