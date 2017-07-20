f44bec62374250c94a87d62e7ddf88643265e3c5
iROCKBUNNY <irockbunny@outlook.com>
2017-07-20 16:11:49 +0800

    Update

--- classify.py
+++ classify.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from common import get_base_dir
+import sys
+
+
+reload(sys)
+sys.setdefaultencoding('utf-8');
+
+
+import os
+import fnmatch
+import json
+from common import BASE_DIR, make_dirs_for_file, exist_file
