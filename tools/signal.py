15d9db8c969983eff5c30470f6e334c28fd913e5
iROCKBUNNY <irockbunny@outlook.com>
2017-07-22 07:35:34 +0800

    Update

--- tools/signal.py
+++ tools/signal.py
@@ -8,9 +8,25 @@ def smooth_ppg(signal, sample_rate=200, numtaps=200, cutoff=[0.5, 5.0]):
     return convolve(signal, firwin(numtaps, [x*2/sample_rate for x in cutoff], pass_zero=False), mode='valid')
 
 
-def segment_ppg(signal):
+def find_extrema(signal):
     import numpy as np
     from scipy.signal import argrelmax, argrelmin
     extrema_index = np.sort(np.unique(np.concatenate((argrelmax(signal)[0], argrelmin(signal)[0]))))
     extrema = signal[extrema_index]
-    return extrema, extrema_index
+    return zip(extrema_index.tolist(), extrema.tolist())
+
+
+def segment_ppg(signal):
+    threshold = (max(signal) - min(signal)) * 0.5
+    segments = []
+    last_extrema_index = None
+    last_extrema = None
+    last_segment_start_index = None
+    for extrema_index, extrema in find_extrema(signal=signal):
+        if last_extrema is not None and extrema - last_extrema > threshold:
+            if last_segment_start_index is not None:
+                segments.append(signal.tolist()[last_segment_start_index:last_extrema_index])
+            last_segment_start_index = last_extrema_index
+        last_extrema_index = extrema_index
+        last_extrema = extrema
+    return segments
