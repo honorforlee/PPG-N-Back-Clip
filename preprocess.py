da05baea5320715aaa1735282c351822eccd6643
iROCKBUNNY <irockbunny@outlook.com>
2017-07-23 02:27:33 +0800

    Update

--- preprocess.py
+++ preprocess.py
@@ -23,11 +23,11 @@ for filename_with_ext in fnmatch.filter(os.listdir(segmented_data_dir), '*.json'
     if json_data is not None:
         for session_id in json_data:
             if json_data[session_id]['rest']['ppg']['signal'] is not None:
-                json_data[session_id]['rest']['ppg']['segments'] = segment_ppg(signal=smooth_ppg(signal=json_data[session_id]['rest']['ppg']['signal']))
+                json_data[session_id]['rest']['ppg']['segments'] = segment_ppg(signal=smooth_ppg(signal=json_data[session_id]['rest']['ppg']['signal'], sample_rate=json_data[session_id]['rest']['ppg']['sample_rate']))
                 del json_data[session_id]['rest']['ppg']['signal']
             for block in json_data[session_id]['blocks']:
                 if block['ppg']['signal'] is not None:
-                    block['ppg']['segments'] = segment_ppg(signal=smooth_ppg(signal=block['ppg']['signal']))
+                    block['ppg']['segments'] = segment_ppg(signal=smooth_ppg(signal=block['ppg']['signal'], sample_rate=block['ppg']['sample_rate']))
                     del block['ppg']['signal']
-    full_output_filename = os.path.join(preprocessed_data_dir, filename_with_ext)
-    dump_json(data=json_data, filename=full_output_filename, overwrite=True)
+        full_output_filename = os.path.join(preprocessed_data_dir, filename_with_ext)
+        dump_json(data=json_data, filename=full_output_filename, overwrite=True)
