e1c0ac9abba7d5c8ba52c3e3f33c90dde901ad6d
iROCKBUNNY <irockbunny@outlook.com>
2017-07-29 13:29:21 +0800

    Update

--- classify.py
+++ classify.py
@@ -20,5 +20,5 @@ if exist(pathname=splited_data_dir):
         pathname = os.path.join(splited_data_dir, filename_with_ext)
         json_data = load_json(pathname=pathname)
         if json_data is not None:
-            score = run_svm(data=json_data, task_levels=['0', '2'], feature_types=['ppg45_cr'])
+            score = run_svm(data=json_data, task_levels=['0', '2'], feature_types=['ppg45_cr', 'svri_cr', 'average_skin_conductance_level_cr', 'minimum_skin_conductance_level_cr', 'average_rri_cr', 'rmssd_cr', 'mf_hrv_power_cr', 'hf_hrv_power_cr'])
             print score
